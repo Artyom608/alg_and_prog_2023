fca8f18d7c24fde90d42b234cc265b1b08d37c62
Artyom608 <66260953+Artyom608@users.noreply.github.com>
2023-09-30 23:19:52 +0300

    Create main.go

---
+++ contest_01/01/02/main.go
@@ -0,0 +1,13 @@
+package main
+import (
+    "fmt"
+    "math"
+)
+func main() {
+    fmt.Println(0.5*365)
+    a:= math.Ceil(182.5/20)
+    b:= math.Ceil(182.5/32)
+    fmt.Println(b)
+    fmt.Println(a)
+    
+}
