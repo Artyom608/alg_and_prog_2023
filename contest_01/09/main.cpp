677d50432978b43ea347847af47476e428104dc7
Artyom608 <66260953+Artyom608@users.noreply.github.com>
2023-09-30 23:35:58 +0300

    Create main.cpp

---
+++ contest_01/09/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    float gipot = 0;
+    float a,b;
+    cin>>a;
+    cin>>b;
+    gipot = sqrt(a*a+b*b);
+    cout<<gipot;    
+}
