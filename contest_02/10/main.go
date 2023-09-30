a4c0a64ae5878abc4370133c962df9f57418f16f
Artyom608 <66260953+Artyom608@users.noreply.github.com>
2023-09-30 23:58:52 +0300

    Create main.go

---
+++ contest_02/10/main.go
@@ -0,0 +1,25 @@
+package main
+import "fmt"
+
+func shift(data []int, s int){
+ var arr_ [10] int
+ s=(s%10+10)%10 
+ for i:=0;i<10;i++{
+  arr_[(i+s)%10] = data[i]   
+ }
+ copy(data,arr_[:])
+}
+
+func main(){
+    var steps int
+    fmt.Scan(&steps)
+
+    var data [10]int
+    for index := range data{
+        fmt.Scan(&data[index])
+    }
+
+    shift(data[:], steps);
+    for _, value := range data{
+        fmt.Printf("%d ", value)
+    }
