b6c45075bdc95fb6763a98b831551ff0044dd8c8
Artyom608 <66260953+Artyom608@users.noreply.github.com>
2023-09-30 23:35:01 +0300

    Create main.cpp

---
+++ contest_01/07/main.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int a, b;
+    scanf("%i%i",&a,&b);
+    if (a>b)
+    {
+        cout<<a;       
+    }
+    else
+    {
+        cout<<b;    
+    }
+}
