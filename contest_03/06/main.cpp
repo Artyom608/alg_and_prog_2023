21935c49d98f2d6070915f1b12ae2d623e5d60e2
Artyom608 <66260953+Artyom608@users.noreply.github.com>
2023-10-13 15:10:18 +0300

    Create main.cpp

---
+++ contest_03/06/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+int main()
+{
+	int count;
+    cin>>count;
+    map<string, string> maps;
+    for(int i = 0; i< count; i++)
+    {
+        string line;
+        cin>>line;
+        int end = line.find(';', 0);
+        maps[line.substr(0,end)] = line.substr(end + 1, 2000);
+    }
+    int count2;
+    cin>>count2;
+    for(int i = 0; i < count2; i++){
+    	string line;
+        cin>>line;
+        cout<<maps[line]<<' ';
+    }    
+}
